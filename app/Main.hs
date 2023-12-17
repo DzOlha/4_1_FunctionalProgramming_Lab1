3b3cd9b1288d4297afb851b9369dfc557adadebd
DzOlha <monsarda11@gmail.com>
2023-12-17 19:03:57 +0200

    Initialize the project and set all dependencies.

---
+++ app/Main.hs
@@ -0,0 +1,21 @@
+
+{-# LANGUAGE OverloadedStrings #-}
+module Main where
+
+import MySQLConnector ( connectDB, deployDB, closeDB )
+import ProgramsTable
+import qualified UIInterface as UI ( doAction )
+
+
+main :: IO ()
+main = do
+
+    conn <- connectDB
+    
+    deployDB conn
+
+    UI.doAction conn
+
+    closeDB conn
+
+
